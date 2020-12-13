dbffb30608010865298e4c6d24b2d481a022297d
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-13 13:07:54 +0100

    feat(crud): add routes, method controller and views to static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,22 @@ class StaticPagesController < ApplicationController
 
   def home
   end
+
+  def legal_notice
+  end
+
+  def cgu_cgv
+  end
+
+  def park_plan
+  end
+
+  def visit_preparation
+  end
+
+  def my_game
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  # get 'static_pages/about'
+  get 'static_pages/legal_notice'
+  get 'static_pages/cgu_cgv'
+  get 'static_pages/visit_preparation'
+  get 'static_pages/park_plan'
+  get 'static_pages/my_game'
+  get 'static_pages/contact'
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
