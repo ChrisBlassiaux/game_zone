a8d91b43734de90bcbac4d20b21a9cdfe314b0c5
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-13 02:18:39 +0100

    feat(database): init migration, model and views with devise gem for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_000636) do
+ActiveRecord::Schema.define(version: 2020_12_13_005536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,24 @@ ActiveRecord::Schema.define(version: 2020_12_13_000636) do
     t.string "closing_times"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "idenfiant"
+    t.integer "xp", default: 0
+    t.boolean "is_admin", default: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
