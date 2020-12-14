dab67dd51f526971fbb034fb3c27aae3b43cfb0c
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-14 23:21:04 +0100

    feat(xp_user): init i18n gem with three btns for fr, en, and es

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module GameZone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.available_locals = [:en, :ru]
+    config.i18n.default_locale = :en
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,30 @@
 Rails.application.routes.draw do
-  resources :avatars
-  resources :news
-  # pages statiques
-  root 'static_pages#home'
-  get 'static_pages/legal_notice'
-  get 'static_pages/cgu_cgv'
-  get 'static_pages/visit_preparation'
-  get 'static_pages/park_plan'
-  get 'static_pages/my_game'
-  get 'static_pages/contact'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 
-  # get 'users/show'
-  get 'users/dashboard_admin'
-  post 'users/add_participant'
-  
-  # pages dynamiques
-  resources :parks, except: [:index, :create, :new]
-  resources :news, except: :index
-  
-  resources :attractions
-  resources :items
+    resources :avatars
+    resources :news
+    # pages statiques
+    root 'static_pages#home'
+    get 'static_pages/legal_notice'
+    get 'static_pages/cgu_cgv'
+    get 'static_pages/visit_preparation'
+    get 'static_pages/park_plan'
+    get 'static_pages/my_game'
+    get 'static_pages/contact'
 
-  devise_for :users
-  resources :users, only: [:show, :edit, :update]
+    # get 'users/show'
+    get 'users/dashboard_admin'
+    post 'users/add_participant'
+    
+    # pages dynamiques
+    resources :parks, except: [:index, :create, :new]
+    resources :news, except: :index
+    
+    resources :attractions
+    resources :items
+
+    devise_for :users
+    resources :users, only: [:show, :edit, :update]
+
+  end
 end
