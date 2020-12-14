a4175f1a44b40728b74e449c422b1b661971b0e6
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-14 14:54:01 +0100

    feat(active_storage): add attach local to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,30 @@ class ItemsController < ApplicationController
   # POST /items
   # POST /items.json
   def create
+    # @user = User.find_by(slug: params[:user_id])
+    # if @user.profile_pic.attached?
+      # @user.profile_pic.purge
+      # if params[:profile_pic]
+        # @user.profile_pic.attach(params[:profile_pic])
+        # flash[:success] = "Vous avez changé votre photo de profil !"
+      # end
+      # flash[:success] = "Vous avez supprimé votre photo de profil !"
+      # redirect_to(user_path(@user))
+    # else
+      # @user.profile_pic.attach(params[:profile_pic])
+      # flash[:success] = "Vous avez changé votre photo de profil !"
+      # redirect_to(user_path(@user))
+    # end
+
     @item = Item.new(item_params)
+    
+
+
+      puts "^" * 50
+      # @item.picture.attach(params[:picture])
+      @item.picture.attach(io: File.open('app/assets/images/favicon.png'), filename: 'favicon.png', content_type: 'image/png')
+      puts @item.picture.attached?
+      puts "^" * 50
 
     respond_to do |format|
       if @item.save
