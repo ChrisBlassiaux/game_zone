4149ad8f278d115b7724d8ffb7fcab9e75b6195e
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-15 00:21:30 +0100

    feat(validates): fix available_locales to application.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  # validates :nickname, uniqueness: true, presence: true, length: {maximum: 30}
+  # validates :password, presence: true,
+  #                      confirmation: true,
+  #                      length: {within: 6..40},
+  #                      on: :create
+  # validates :password, confirmation: true,
+  #                      length: {within: 6..40},
+  #                      allow_blank: true,
+  #                      on: :update
+  # validates :description, length: {maximum: 350}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
