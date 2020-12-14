4083343b503a86f77b4c6905c2618b0a89146c28
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-14 17:56:15 +0100

    feat(active_storage): active_storage local to correct

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,31 +26,14 @@ class ItemsController < ApplicationController
   # POST /items
   # POST /items.json
   def create
-    # @user = User.find_by(slug: params[:user_id])
-    # if @user.profile_pic.attached?
-      # @user.profile_pic.purge
-      # if params[:profile_pic]
-        # @user.profile_pic.attach(params[:profile_pic])
-        # flash[:success] = "Vous avez changé votre photo de profil !"
-      # end
-      # flash[:success] = "Vous avez supprimé votre photo de profil !"
-      # redirect_to(user_path(@user))
-    # else
-      # @user.profile_pic.attach(params[:profile_pic])
-      # flash[:success] = "Vous avez changé votre photo de profil !"
-      # redirect_to(user_path(@user))
-    # end
 
     @item = Item.new(item_params)
     
-
-
-      puts "^" * 50
-      # @item.picture.attach(params[:picture])
-      @item.picture.attach(io: File.open('app/assets/images/favicon.png'), filename: 'favicon.png', content_type: 'image/png')
-      puts @item.picture.attached?
-      puts "^" * 50
-
+    # puts "^" * 50
+    # @item.picture.attach(io: File.open('app/assets/images/favicon.png'), filename: 'favicon.png', content_type: 'image/png')
+    # puts @item.picture.attached?
+    # puts "^" * 50
+    
     respond_to do |format|
       if @item.save
         format.html { redirect_to @item, notice: 'Item was successfully created.' }
@@ -60,6 +43,7 @@ class ItemsController < ApplicationController
         format.json { render json: @item.errors, status: :unprocessable_entity }
       end
     end
+    @item.picture.attach(params[:picture])
   end
 
   # PATCH/PUT /items/1
@@ -94,6 +78,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :description, :unit_price_ht, :unit_price_tva, :unit_price_ttc, :tva)
+      params.require(:item).permit(:name, :description, :unit_price_ht, :unit_price_tva, :unit_price_ttc, :tva, :picture)
     end
 end
