0463198e6579f31b3f2023629dbda07c450392c0
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-19 21:46:11 +0100

    feat(front): init front contact section

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -59,38 +59,8 @@ window.onload = function () {
 
   };
 
-  inputs[y].addEventListener('keypress');
-
-
   // go_to_the_park
-  navigator.geolocation.getCurrentPosition(showPosition);
-
-let longitude = 0;
-let latitude = 0;
-
-function showPosition(position) {
-	latitude = position.coords.latitude;
-	longitude =position.coords.longitude;
-
-	let carte = L.map("carte").setView([48.852969, 2.349903],13)
-	L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',).addTo(carte)
-	L.routing.control({
-		waypoints: [
-			L.latLng(latitude, longitude ),
-			L.latLng(48.871900, 2.776623)
-	],
-		geocoder: L.Control.Geocoder.nominatim(),
-		lineOptions: {
-			styles: [{
-				color: '#C9116A',
-				opacity: 1,
-			}]
-		},
-		router: new L.Routing.osrmv1({
-			language: 'fr'
-		})
-	}).addTo(carte)
-} 
+ 
 
 
 // calendar 
