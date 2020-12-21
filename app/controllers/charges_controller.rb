353f8afef85dd4c83e190c3a4e7c324d4795c0ae
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-12-21 21:45:15 +0100

    feat(front): init front cart page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,20 +8,22 @@ class ChargesController < ApplicationController
     @cart = current_order
     @amount = (@cart.subtotal * 100).to_i
   
-    customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-    })
+    # customer = Stripe::Customer.create({
+    #   email: params[:stripeEmail],
+    #   source: params[:stripeToken],
+    # })
   
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'eur',
-    })
+    # charge = Stripe::Charge.create({
+    #   customer: customer.id,
+    #   amount: @amount,
+    #   description: 'Rails Stripe customer',
+    #   currency: 'eur',
+    # })
 
-    @order = Order.create(number_order: 4584472255445583, state: 'En cours de préparation', delivery_mode: 'À domicile', delivery_address: '70 Rue Jacques Prévers', amount_ht: 10.00, amount_ttc: 12.00, user_id: User.first.id, cart_id: Cart.first.id)
+    # @order = Order.create(number_order: 4584472255445583, state: 'En cours de préparation', delivery_mode: 'À domicile', delivery_address: '70 Rue Jacques Prévers', amount_ht: 10.00, amount_ttc: 12.00, user_id: User.first.id, cart_id: Cart.first.id)
   
+    OrderItem.destroy_all
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
